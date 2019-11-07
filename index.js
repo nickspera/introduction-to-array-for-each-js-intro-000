c42a963db6e33c146d54cb881a6691a3c91218c4
Nick Spera <nickaspera@gmail.com>
2019-11-07 22:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ var animals = ["dog", "cat", "squirrel"];
 doToElementsInArray(animals, changeCompletely);
 
 // log out animals -- pretty cool, right?
-console.log(animals);
+// console.log(animals);
