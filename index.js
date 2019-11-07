88f0a29ac588879dd4a24bcd941505c1e72b16b7
Nick Spera <nickaspera@gmail.com>
2019-11-07 22:04:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Add your doToElementsInArray() function here:
 
+function doToElementsInArray(array, callback){
+  array.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
+
+function changeCompletely(element, index, array) {
+  array[index] = `${array[index]}s!`;
+}
