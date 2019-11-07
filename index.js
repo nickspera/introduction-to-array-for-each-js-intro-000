329d67c8783e054cef7ba54122c0f6e8ed57fbb7
Nick Spera <nickaspera@gmail.com>
2019-11-07 21:31:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your doToElementsInArray() function here:
 
 doToElementsInArray(array, callback){
-  array.forEach(callback);
+  array.prototype.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
