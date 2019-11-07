cdc2b8268a8ff7c19aff18f56022b56b6e372d65
Nick Spera <nickaspera@gmail.com>
2019-11-07 21:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function doToElementsInArray(array, callback){
 }
 
 // Add your changeCompletely() function here:
+
+function changeCompletely(element, index, array) {
+    array.prototype.forEach();
+}
